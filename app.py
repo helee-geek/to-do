22ab0a4d7c1d9a158a524e2d556c3a548fa9d44d
helee-geek <heleergeek435@gmail.com>
2026-06-16 15:27:55 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask inport Flask, request, jsonify
+
+app = Flask(__name__);
+
+# in memory database
+lists = []
+task_id = 1
+
+# create task - POST /tasks
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id
+    data = request.get_json()
+    task = {
+        'id': task_id,
+        'title': data['title'],
+        'description': data.get('description', ''),
+        'done': False
+    }
+    lists.append(task)
+    task_id += 1
+    return jsonify(task), 201
